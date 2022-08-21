f2aee8485a21c27d25462b186fef9b0ed02133a7
junylee <junylee@junyleeui-MacBookPro.local>
2022-08-21 18:47:39 +0900

    Module-02 done

--- CPP-Module-02/ex02/Fixed.cpp
+++ CPP-Module-02/ex02/Fixed.cpp
@@ -5,42 +5,112 @@ const int Fixed::_bits = 8;
 Fixed::Fixed(void)
 {
 	_value = 0;
-	std::cout << "Default constructor called" << std::endl;
 }
 
 Fixed::Fixed(const int value)
 {
 	_value = value << _bits;
-	std::cout << "Int constructor called" << std::endl;	
 }
 
 Fixed::Fixed(const float value)
 {
-	_value = round(value * (1 << _bits));
-	std::cout << "Float constructor called" << std::endl;
+	_value = roundf(value * (1 << _bits));
 }
 
 Fixed::Fixed(const Fixed &src)
 {
-	std::cout << "Copy constructor called" << std::endl;
 	*this = src;
 }
 
 Fixed::~Fixed(void)
 {
-	std::cout << "Destructor called" << std::endl;
 }
 
 Fixed& Fixed::operator=(Fixed const &src)
 {
-	std::cout << "Copy assignment operator called" << std::endl;
 	_value = src._value;
 	return (*this);
 }
 
+bool	Fixed::operator>(Fixed const &src) const
+{
+	return (this->_value > src._value);
+}
+		
+bool	Fixed::operator<(Fixed const &src) const
+{
+	return (this->_value < src._value);
+}
+
+bool	Fixed::operator>=(Fixed const &src) const
+{
+	return (this->_value >= src._value);
+}
+	
+bool	Fixed::operator<=(Fixed const &src) const
+{
+	return (this->_value <= src._value);
+}
+
+bool	Fixed::operator==(Fixed const &src) const
+{
+	return (this->_value == src._value);
+}
+	
+bool	Fixed::operator!=(Fixed const &src) const
+{
+	return (this->_value != src._value);
+}
+	
+Fixed	Fixed::operator+(Fixed const &src) const
+{
+	return (Fixed(this->toFloat() + src.toFloat()));
+}
+	
+Fixed	Fixed::operator-(Fixed const &src) const
+{
+	return (Fixed(this->toFloat() - src.toFloat()));
+}
+		
+Fixed	Fixed::operator*(Fixed const &src) const
+{
+	return (Fixed(this->toFloat() * src.toFloat()));
+}
+
+Fixed	Fixed::operator/(Fixed const &src) const
+{
+	return (Fixed(this->toFloat() / src.toFloat()));
+}
+		
+Fixed&	Fixed::operator++(void)
+{
+	this->_value++;
+	return (*this);
+}
+
+Fixed	Fixed::operator++(int)
+{
+	Fixed f(*this);
+	this->_value++;
+	return (f);
+}
+	
+Fixed&	Fixed::operator--(void)
+{
+	this->_value--;
+	return (*this);
+}
+	
+Fixed	Fixed::operator--(int)
+{
+	Fixed f(*this);
+	this->_value--;
+	return (f);
+}
+
+
 int Fixed::getRawBits(void) const
 {
-	std::cout << "getRawBits member function called" << std::endl;
 	return (_value);
 }
 
@@ -58,6 +128,35 @@ float	Fixed::toFloat(void) const
 {
 	return (float(_value) / (1 << _bits));
 }
+
+Fixed&	Fixed::min(Fixed &f1, Fixed &f2)
+{
+	if (f1 < f2)
+		return (f1);
+	return (f2);
+}
+
+Fixed const&	Fixed::min(Fixed const &f1, Fixed const &f2)
+{
+	if (f1 < f2)
+		return (f1);
+	return (f2);
+}
+
+Fixed&	Fixed::max(Fixed &f1, Fixed &f2)
+{
+	if (f1 > f2)
+		return (f1);
+	return (f2);
+}
+
+Fixed const&	Fixed::max(Fixed const &f1, Fixed const &f2)
+{
+	if (f1 > f2)
+		return (f1);
+	return (f2);
+}
+
 
 std::ostream& operator<<(std::ostream &s, const Fixed &fixed)
 {
--- CPP-Module-02/ex02/Fixed.hpp
+++ CPP-Module-02/ex02/Fixed.hpp
@@ -23,21 +23,26 @@ class Fixed
 		Fixed	operator-(Fixed const &src) const;
 		Fixed	operator*(Fixed const &src) const;
 		Fixed	operator/(Fixed const &src) const;
+		Fixed&	operator++(void);
+		Fixed	operator++(int);	
+		Fixed&	operator--(void);
+		Fixed	operator--(int);
+
 		int getRawBits(void) const;
 		void setRawBits(int const raw);
 
 		float toFloat(void) const;
 		int toInt(void) const;
 
-		static Fixed &min(Fixed &f1, Fixed &f2);
-		static Fixed const &min(Fixed const &f1, Fixed const &f2);
-		static Fixed &max(Fixed &f1, Fixed &f2);
-		static Fixed const &max(Fixed const &f1, Fixed const &f2);
+		static Fixed& min(Fixed &f1, Fixed &f2);
+		static Fixed const& min(Fixed const &f1, Fixed const &f2);
+		static Fixed& max(Fixed &f1, Fixed &f2);
+		static Fixed const& max(Fixed const &f1, Fixed const &f2);
 	private:
 		int	_value;
 		const static int _bits;
 };
 
-std::ostream& operator<<(std::ostream &s, const Fixed & fixed);
+std::ostream& operator<<(std::ostream &s, const Fixed& fixed);
 
 #endif

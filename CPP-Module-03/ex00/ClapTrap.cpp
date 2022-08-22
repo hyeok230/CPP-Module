43db2ba655d10bf5ea8abb18e4b552a09ea2b5ec
Yub_2 <hyeok230@naver.com>
2022-08-22 22:11:01 +0900

    Module 03 ex00 ing

--- CPP-Module-03/ex00/ClapTrap.cpp
+++ CPP-Module-03/ex00/ClapTrap.cpp
@@ -1,11 +1,16 @@
 #include "ClapTrap.hpp"
 
-ClapTrap::ClapTrap(void)
+ClapTrap::ClapTrap(void) : _name("defult"), _hitPoints(10), _energyPoints(10), _attackDamage(0)
 {
 	std::cout << "Default constructor called" << std::endl;
 }
 
 ClapTrap::ClapTrap(std::string name) : _name(name), _hitPoints(10), _energyPoints(10), _attackDamage(0)
+{
+	std::cout << "String constructor called" << std::endl;
+}
+
+ClapTrap::ClapTrap(ClapTrap const & src)
 {
 	
 }

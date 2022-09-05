e9701a582435a54c074cd87a6b6121611c1b0775
hyeok230 <hyeok230@naver.com>
2022-09-05 18:24:42 +0900

    Module 07 ing

---
+++ CPP-Module-07/ex00/whatever.hpp
@@ -0,0 +1,18 @@
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+# include <iostream>
+
+template <typename T>
+void swap(T &a, T &b)
+{
+    T tmp;
+
+    tmp = a;
+    a = b;
+    b = tmp;
+}
+
+template <typename T>
+
+#endif

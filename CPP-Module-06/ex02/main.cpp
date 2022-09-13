129271a4efc53da371b8f3b4cafca14fc79e6887
Yub_2 <hyeok230@naver.com>
2022-09-13 18:12:16 +0900

    need to fix

--- CPP-Module-06/ex02/main.cpp
+++ CPP-Module-06/ex02/main.cpp
@@ -32,12 +32,8 @@ void identify(Base *p)
 
 void	identify(Base& p)
 {
-	if (dynamic_cast<A *>(&p))
-		std::cout << "A" << std::endl;
-	else if (dynamic_cast<B *>(&p))
-		std::cout << "B" << std::endl;
-	else if (dynamic_cast<C *>(&p))
-		std::cout << "C" << std::endl;
+		
+
 }
 
 int main()

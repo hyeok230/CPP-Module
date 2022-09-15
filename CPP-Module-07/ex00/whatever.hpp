9e994f1ec2746db5ab7abbecfdf0b36138129f4f
Yub_2 <hyeok230@naver.com>
2022-09-15 20:22:30 +0900

    fix const error

--- CPP-Module-07/ex00/whatever.hpp
+++ CPP-Module-07/ex00/whatever.hpp
@@ -14,13 +14,25 @@ void swap(T &a, T &b)
 }
 
 template <typename T>
-const T& min(const T &a, const T &b)
+T& min(const T &a, const T &b)
 {
     return (a < b) ? a : b; 
 }
 
 template <typename T>
-const T& max(const T &a, const T &b)
+T& min(T &a, T &b)
+{
+    return (a < b) ? a : b; 
+}
+
+template <typename T>
+T& max(const T &a, const T &b)
+{
+   return (a > b) ? a : b; 
+}
+
+template <typename T>
+T& max(T &a, T &b)
 {
    return (a > b) ? a : b; 
 }
